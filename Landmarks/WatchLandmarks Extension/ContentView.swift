d36dcfeee6290362f63455c961ce54bc78fb2c84
freak_lather <ajaysinghlather@gmail.com>
2020-04-27 17:40:00 +0800

    initial commit

---
+++ Landmarks/WatchLandmarks Extension/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Extension
+//
+//  Created by ajay lather on 27/04/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList { WatchLandmarkDetail(landmark: $0) }
+        .environmentObject(UserData())
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList { WatchLandmarkDetail(landmark: $0) }
+        .environmentObject(UserData())
+    }
+}
---
+++ Landmarks/WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by ajay lather on 27/04/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
